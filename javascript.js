5c34175718dbfe04c7c3621e88d96ee5a8c4e16c
GunnalaRajesh <96969851+GunnalaRajesh@users.noreply.github.com>
2023-08-19 14:20:13 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
 function playerdices(){
     randomNumber1 =  Math.floor(Math.random()*6);
-    playerdice_1 = array[randomNumber1];
-    return playerdice_1;
+    return randomNumber1;
 }
 
-
 var array = ["./dice1.png","./dice2.png","./dice3png","./dice4.png","./dice5.png","./dice6.png"]
-
-pd1 = playerdices();
-pd2 = playerdices();
+random1 = playerdices();
+random2 = playerdices();
+pd1 = array[random1];
+pd2 = array[random2];
 document.querySelector(".img1").setAttribute("src",pd1);
 document.querySelector(".img2").setAttribute("src",pd2);
+document.querySelector(".img1").setAttribute("alt",random1);
+document.querySelector(".img2").setAttribute("alt",random2);
 
 if(pd1<pd2){
     document.querySelector("h1").textContent = "Player2 wins";
